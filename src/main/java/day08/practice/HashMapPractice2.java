9b4a0cf556756eca8311cb6506cc6e12336c307d
isac <116252006+isacdevaabishek@users.noreply.github.com>
2023-07-18 00:51:22 +0530

    day07 updated

---
+++ src/main/java/day08/practice/HashMapPractice2.java
@@ -0,0 +1,21 @@
+package day08.practice;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Scanner;
+
+
+public class HashMapPractice2 {
+	public static void main(String[] args) {
+		
+		Holdmethods.addMethods();
+		Holdmethods.View();
+		
+	}
+}
+
+
+
+
+
+
---
+++ src/main/java/day08/practice/HashWorks.java
@@ -0,0 +1,31 @@
+package day08.practice;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class HashWorks{
+	static Map <String,Integer> countMap=new TreeMap<String,Integer>();//{}
+	static boolean getarr(String [] arr) throws IllegalArgumentException{
+		
+		if(arr==null) {
+			throw new IllegalArgumentException("Array Cant Be Null");
+		}
+		for(int i=0;i<arr.length;i++) {
+			if(countMap.get(arr[i].toLowerCase())==null) {
+				
+				countMap.put(arr[i].toLowerCase(), 1); 
+			}
+			else {
+				int count=countMap.get(arr[i].toLowerCase());
+				count++;
+				countMap.put(arr[i].toLowerCase(), count);
+			}
+		}
+		return true;
+	}
+	
+	static boolean viewMap() { 
+		System.out.println(countMap);
+		return true;
+	}
+} 
